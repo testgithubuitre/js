4b0b1355dc4ff5bf6a1298fb69071f996031cf98
testgithubuitre <166409575+testgithubuitre@users.noreply.github.com>
2024-04-16 06:58:05 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+"Use Strict"; // treat all js code as a newer vrsion
+//alert(3+3)  we are using node js , not browser 
+
+
+let name = "Diksha";
+let age = "21";
+let isLoggedin = "False";
+let state;
+
+//type of datatypes
+// 1 . Number = 2 to power 53;
+// 2. bigInt;
+// 3. string = ""
+// 4. bollean =  "true or false"
+// 5. null = "Standalone value"
+// 6. undefined = "value unasigned"
+// 7. symbol = "Unique value"
+
+// 8. object
+
+
+console.log(typeof "null"); //object
+console.log(typeof "undefined"); // undefined
